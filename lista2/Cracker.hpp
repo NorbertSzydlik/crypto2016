cf5853dcde710a17c3a17605233f0deb7d86c20c
Norbert Szydlik <kefaise@gmail.com>
2016-10-29 10:14:53 +0200

    Optimize cracker a bit

--- lista2/Cracker.hpp
+++ lista2/Cracker.hpp
@@ -4,112 +4,124 @@
 #include <iterator>
 #include <string>
 #include <algorithm>
+#include <atomic>
+#include <cctype>
 #include <boost/format.hpp>
 #include "utils.hpp"
 
 class Cracker
 {
-using Keys = std::vector<Key>;
+  using Keys = std::vector<Key>;
 public:
-    Cracker(unsigned int keyLength, const std::string& suffix, const Iv& iv) :
-        keyLength_(keyLength),
-        suffix_(suffix),
-        iv_(iv),
-        numOfThreads_(1)//std::thread::hardware_concurrency())
-    {
-        assert(suffix.size() < keyLength);
-        suffixNum_ = Key(numberFromBytes(std::begin(suffix), std::end(suffix)));
-        maxKey_ = std::string(keyLength - suffix.size(), 'f') + suffix_;
-        assert(maxKey_.size() == keyLength);
-    }
+  Cracker(unsigned int keyLength, const std::string& suffix, const Iv& iv) :
+  keyLength_(keyLength),
+  suffix_(suffix),
+  iv_(iv),
+  numOfThreads_(std::thread::hardware_concurrency())
+  {
+    assert(suffix.size() < keyLength);
+    suffixBuf_ = toBytes(boost::multiprecision::cpp_int("0x" + suffix), keyLength / 2);
 
-    Keys crack(const ByteBuffer& ciphertext)
-    {
-        const int blockSize = EVP_CIPHER_block_size(EVP_aes_256_cbc());
-        ciphertext_ = ciphertext;
-        std::copy(std::begin(ciphertext_), std::begin(ciphertext_) + blockSize, std::back_inserter(ivCiphertext_));
-        keys_ = {};
+    suffixBitSize_ = suffix.size() * 4;
+  }
 
-        std::cout << "Cracking using " << numOfThreads_ << " threads" << std::endl;
-        std::vector<std::shared_ptr<std::thread>> threads;
-        for(int i = 0; i < numOfThreads_; ++i)
-        {
-            auto t = std::make_shared<std::thread>([this, i]() {
-                crackThread(i);
-            });
-            threads.push_back(t);
-        }
-        for(auto& t : threads)
-        {
-            t->join();
-        }
-        return keys_;
+  Keys crack(const ByteBuffer& ciphertext)
+  {
+    const int blockSize = EVP_CIPHER_block_size(EVP_aes_256_cbc());
+    ciphertext_ = ciphertext;
+    std::copy(std::begin(ciphertext_), std::begin(ciphertext_) + (blockSize), std::back_inserter(ivCiphertext_));
+    keys_ = {};
+    working_ = true;
+    lastPrint_.store(0);
+    previousPrintTime_ = std::chrono::system_clock::now();
+
+    std::cout << "Cracking using " << numOfThreads_ << " threads" << std::endl;
+    std::vector<std::shared_ptr<std::thread>> threads;
+    for(int i = 0; i < numOfThreads_; ++i)
+    {
+      auto t = std::make_shared<std::thread>([this, i]() {
+        crackThread(i);
+      });
+      threads.push_back(t);
     }
+    for(auto& t : threads)
+    {
+      t->join();
+    }
+    return keys_;
+  }
 private:
-    void insertKey(const Key& key)
-    {
-        std::lock_guard<std::mutex> lock(possibleKeyInsertionMutex_);
-        keys_.push_back(key);
-    }
-    std::string getFullKeyStr(const Key& prefix)
-    {
-        std::string prefixHex = (boost::format("%1$x") % prefix).str();
-        auto zeroesNeeded = (int)keyLength_ - (int)prefixHex.size() - (int)suffix_.size();
-        std::string zeroes = zeroesNeeded > 0 ? std::string(zeroesNeeded, '0') : "";
-        auto keyHex = zeroes + prefixHex + suffix_;
-        std::transform(std::begin(keyHex), std::end(keyHex), std::begin(keyHex), ::tolower);
+  void insertKey(const Key& key)
+  {
+    std::lock_guard<std::mutex> lock(possibleKeyInsertionMutex_);
+    keys_.push_back(key);
+  }
+  void printSpeed()
+  {
+    const auto printEvery = 1000000;
 
-        return keyHex;
-    }
-    Key getFullKey(const Key& prefix)
-    {
-        const auto keyHex = getFullKeyStr(prefix);
-        return Key("0x" + keyHex);
-    }
-    void crackThread(int s)
-    {
-       Key prefix = s;
-       auto fullKeyStr = getFullKeyStr(prefix);
-       while(fullKeyStr.size() <= keyLength_)
-       {
-           auto fullKey = getFullKey(prefix);
-           if(prefix % (1024 * 8) == 0) std::cout << "current key:'" << fullKeyStr << "'" << std::endl;
-           try
-           {
-               auto possiblePlaintext = decrypt(ciphertext_, fullKey, iv_);
-               if(isValid(possiblePlaintext)) {
-                   std::cout
-                       << "FOUND! possiblePlaintext:'" << possiblePlaintext
-                       << "', fullKeyStr:'" << fullKeyStr << "'"
-                       << std::endl;
-                   insertKey(fullKey);
-               }
-           }
-           catch(...)
-           {
-           }
-           prefix += numOfThreads_;
-           fullKeyStr = getFullKeyStr(prefix);
-       }
-    }
-    bool isValid(const std::string& possiblePlaintext)
-    {
-        //std::cout << "testing: '" << possiblePlaintext << "'" << std::endl;
-        return std::all_of(std::begin(possiblePlaintext), std::end(possiblePlaintext), [](const auto& c) {
-            //std::cout << "current: " << (int)c << " " << c << std::endl;
-            return (c >= 32 && c <= 126) || c == 10 || c == 13 || c == 0;
-        });
-    }
+    if(lastPrint_.fetch_add(1) % printEvery != 0) return;
 
-    unsigned int keyLength_;
-    unsigned int suffixLength_;
-    std::string suffix_;
-    Key suffixNum_;
-    std::string maxKey_;
-    Iv iv_;
-    std::mutex possibleKeyInsertionMutex_;
-    Keys keys_;
-    ByteBuffer ciphertext_;
-    ByteBuffer ivCiphertext_;
-    const uint64_t numOfThreads_;
+    auto now = std::chrono::system_clock::now();
+    auto elapsedSeconds = std::chrono::duration_cast<std::chrono::duration<float, std::ratio<1,1>>>(now - previousPrintTime_).count();
+
+    std::cout << "speed: " << ((float)printEvery/elapsedSeconds) << "/s" << std::endl;
+
+    previousPrintTime_ = now;
+  }
+  void crackThread(int s)
+  {
+    Key key = suffixBuf_;
+
+    addOnBit(key, suffixBitSize_, s);
+
+    while(key.size() <= (keyLength_ / 2) && working_)
+    {
+      printSpeed();
+      try
+      {
+        auto possiblePlaintext = decrypt(ivCiphertext_, key, iv_);
+        //if(isValid(possiblePlaintext)) {
+          //auto possiblePlaintext = decrypt(ciphertext_, key, iv_);
+          if(isValid(possiblePlaintext))
+          {
+            std::cout
+            << "FOUND! possiblePlaintext:'" << possiblePlaintext
+            << "', fullKeyStr:'" << hex(key) << "'"
+            << std::endl;
+            insertKey(key);
+            working_ = false;
+          }
+        //}
+      }
+      catch(...)
+      {
+      }
+      addOnBit(key, suffixBitSize_, numOfThreads_);
+    }
+  }
+  bool isValid(const std::string& possiblePlaintext)
+  {
+    //std::cout << "testing: '" << possiblePlaintext << "'" << std::endl;
+    return std::all_of(std::begin(possiblePlaintext), std::end(possiblePlaintext), [](const auto& c) {
+      //std::cout << "current: " << (int)c << " " << c << std::endl;
+      return std::isgraph(c) || std::isspace(c);
+    });
+  }
+
+  unsigned int keyLength_;
+  unsigned int suffixLength_;
+  Key suffixBuf_;
+  unsigned suffixBitSize_;
+  std::string suffix_;
+  Iv iv_;
+  std::mutex possibleKeyInsertionMutex_;
+  Keys keys_;
+  ByteBuffer ciphertext_;
+  ByteBuffer ivCiphertext_;
+  const uint64_t numOfThreads_;
+  bool working_;
+
+  std::atomic<int> lastPrint_;
+  std::chrono::time_point<std::chrono::system_clock> previousPrintTime_;
 };
--- lista2/utils.hpp
+++ lista2/utils.hpp
@@ -2,13 +2,32 @@
 #include <vector>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/cpp_dec_float.hpp>
+#include <openssl/evp.h>
+#include <openssl/err.h>
 
 #include "CipherCtxHandler.hpp"
 
 using ByteBuffer = std::vector<uint8_t>;
-using Key = boost::multiprecision::uint256_t;
-using Iv = boost::multiprecision::uint128_t;
+using Key = ByteBuffer;
+using Iv = ByteBuffer;
 const auto AES_BITS = 256;
+
+void addOnByte(ByteBuffer& buf, int byte, int value)
+{
+  if(value == 0) return;
+  if(buf.size() <= byte)
+  {
+    buf.resize(byte + 1);
+  }
+  auto currentByte = (std::rbegin(buf) + byte);
+  uint32_t newValue = *currentByte + value;
+  *currentByte = newValue % 256;
+  addOnByte(buf, byte + 1, newValue / 256);
+}
+void addOnBit(ByteBuffer& buf, int bit, int value)
+{
+  addOnByte(buf, bit / 8, value << (bit % 8));
+}
 
 std::string hex(const ByteBuffer& b, bool splitWithSpaces)
 {
@@ -41,14 +60,12 @@ boost::multiprecision::cpp_int numberFromBytes(const ForwardIterator& begin, con
     return num;
 }
 
-ByteBuffer encrypt(const std::string& plaintextStr, const Key& keyNum, const Iv& ivNum)
+ByteBuffer encrypt(const std::string& plaintextStr, const Key& key, const Iv& iv)
 {
     const int blockSize = EVP_CIPHER_block_size(EVP_aes_256_cbc());
 
     ByteBuffer plaintext;
     std::copy(std::begin(plaintextStr), std::end(plaintextStr), std::back_inserter(plaintext));
-    auto key = toBytes(keyNum, AES_BITS / 8);
-    auto iv = toBytes(ivNum, AES_BITS / 16);
 
     ByteBuffer ciphertext(plaintext.size() + blockSize);
 
@@ -83,12 +100,9 @@ ByteBuffer encrypt(const std::string& plaintextStr, const Key& keyNum, const Iv&
     return ciphertext;
 }
 
-std::string decrypt(const ByteBuffer& ciphertext, const Key& keyNum, const Iv& ivNum)
+std::string decrypt(const ByteBuffer& ciphertext, const Key& key, const Iv& iv)
 {
     CipherCtxHandler ctx;
-
-    auto key = toBytes(keyNum, AES_BITS / 8);
-    auto iv = toBytes(ivNum, AES_BITS / 16);
 
     ByteBuffer plaintext(ciphertext.size(), 0);
     int len;
@@ -100,20 +114,26 @@ std::string decrypt(const ByteBuffer& ciphertext, const Key& keyNum, const Iv& i
      * IV size for *most* modes is the same as the block size. For AES this
      * is 128 bits */
     if(1 != EVP_DecryptInit_ex(&ctx, EVP_aes_256_cbc(), NULL, key.data(), iv.data()))
+    {
         throw std::runtime_error("decrypt Failed init");
-
+    }
     /* Provide the message to be decrypted, and obtain the plaintext output.
      * EVP_DecryptUpdate can be called multiple times if necessary
      */
     if(1 != EVP_DecryptUpdate(&ctx, plaintext.data(), &len, ciphertext.data(), ciphertext.size()))
+    {
         throw std::runtime_error("decrypt Failed update");
+    }
     plaintext_len = len;
 
     /* Finalise the decryption. Further plaintext bytes may be written at
      * this stage.
      */
-    if(1 != EVP_DecryptFinal_ex(&ctx, plaintext.data() + len, &len)) throw std::runtime_error("decrypt Failed final");
-        plaintext_len += len;
+    if(1 != EVP_DecryptFinal_ex(&ctx, plaintext.data() + len, &len))
+    {
+        throw std::runtime_error("decrypt Failed final");
+    }
+    plaintext_len += len;
 
     plaintext.resize(plaintext_len);
 
